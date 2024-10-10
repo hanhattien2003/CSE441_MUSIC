90447990c9d33a4206724d878c38ab5d8a8c541c
Quốc Tuấn Phạm <qtuan.ph@outlook.com>
2024-10-10 14:02:03 +0700

    Sua 1 chut code

---
+++ app/src/main/java/com/example/cse441_music/Controller/MusicController.java
@@ -0,0 +1,5 @@
+package com.example.cse441_music.Controller;
+
+public class MusicController {
+    // Lam cac chuc nang: Them nhac vao danh sach yeu thich, tim kiem, phat nhac, ...
+}
---
+++ app/src/main/java/com/example/cse441_music/Controller/MusicPlayerController.java
@@ -0,0 +1,20 @@
+package com.example.cse441_music.Controller;
+
+public class MusicPlayerController {
+    // Lam cac chuc nang dieu khien nhac
+    public void play() {
+        // phat nhac
+    }
+    public void pause() {
+        // dung nhac
+    }
+    public void stop() {
+        // dung nhac
+        }
+    public void next() {
+        // phat nhac tiep theo
+    }
+    public void previous() {
+        // phat nhac truoc do
+    }
+}

3fc0bfbeacf77e1b5dbd2c2f7bd37b3624b7c9d9
Quốc Tuấn Phạm <qtuan.ph@outlook.com>
2024-10-29 14:48:43 +0700

    update model Song

--- app/src/main/java/com/example/cse441_music/Model/Song.java
+++ app/src/main/java/com/example/cse441_music/Model/Song.java
@@ -34,31 +34,134 @@ public class Song {
 
     }
 
-    public String getId() {
-        return id;
+    public Song() {
     }
 
-    public String getName() {
-        return name;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getArtistName() {
-        return artistName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getAudioUrl() {
-        return audioUrl;
+    public String getDuration() {
+        return duration;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 
-    public String getAlbumName() {
-        return albumName;
+    public String getArtistId() {
+        return artistId;
     }
 
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
 
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
 
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
 
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getAlbumImage() {
+        return albumImage;
+    }
+
+    public void setAlbumImage(String albumImage) {
+        this.albumImage = albumImage;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+
+    public String getAudioDownloadUrl() {
+        return audioDownloadUrl;
+    }
+
+    public void setAudioDownloadUrl(String audioDownloadUrl) {
+        this.audioDownloadUrl = audioDownloadUrl;
+    }
+
+    public String getProUrl() {
+        return proUrl;
+    }
+
+    public void setProUrl(String proUrl) {
+        this.proUrl = proUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getShareUrl() {
+        return shareUrl;
+    }
+
+    public void setShareUrl(String shareUrl) {
+        this.shareUrl = shareUrl;
+    }
+
+    public String getWaveform() {
+        return waveform;
+    }
+
+    public void setWaveform(String waveform) {
+        this.waveform = waveform;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isAudioDownloadAllowed() {
+        return audioDownloadAllowed;
+    }
+
+    public void setAudioDownloadAllowed(boolean audioDownloadAllowed) {
+        this.audioDownloadAllowed = audioDownloadAllowed;
+    }
 }
